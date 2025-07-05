223edf4f5a177f1c44892b8f490d5b33fbf548c4
BrandonAlexisDev22 <brandonalexisquintero.sena@gmail.com>
2025-07-05 15:01:21 -0500

    Change files

--- static/js/reservas.js
+++ static/js/reservas.js
@@ -613,4 +613,4 @@ document.addEventListener('DOMContentLoaded', function() {
                fecha1.getMonth() === fecha2.getMonth() &&
                fecha1.getDate() === fecha2.getDate();
     }
-}); 
+});
